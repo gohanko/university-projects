81e5cfd3d961f8046238706b6489cf83ae196a0e
gohanko <26451183+gohanko@users.noreply.github.com>
2021-09-08 23:57:49 +0800

    added todos

---
+++ automated_unitreg/course_selection_method.py
@@ -0,0 +1,13 @@
+'''TODO: implement different methods of selecting courses'''
+
+def packed_on_start_of_week():
+    '''Tries to cram as much courses in the begining of the week as possible'''
+    pass
+
+def spread_out():
+    '''Tries to spread the courses evenly throughout the week'''
+    pass
+
+def relax_a_day():
+    '''Tries to schedule course with alternating days of highload and lowload.'''
+    pass
---
+++ automated_unitreg/selenium_unitreg/parsers.py
@@ -0,0 +1,3 @@
+# TODO: should parse the course table into a dict containing all relevant information.
+def parse_course_table():
+    pass
