b3bd5956adefc13bfaa1d576f49cf8bc0585a51d
gohanko <26451183+gohanko@users.noreply.github.com>
2021-11-26 12:45:11 +0800

    removed submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
+#include <SFML\Window.hpp>
+class Board {
 
+};
+
+class Piece {
+
+
+};
+
+class MenuGUI {
+
+};
 
 int main() {
+    sf::Window window(sf::VideoMode(800, 600), "Moe Chess!");
 
-}
+    while (window.isOpen()) {
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close()
+            }
+        }
+    }
+
+    return 0
+};
