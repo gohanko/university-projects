bc5c17066c0a44dc58e25eeafb2f60b266074446
gohanko <26451183+gohanko@users.noreply.github.com>
2021-11-27 15:20:15 +0800

    Added board

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Anime themed and kawaii chess game written in C++
 To get started, please have the following installed:
 
 - GCC 7.3.0 MinGW (SEH) - 64-bit
+
+## Credit
+[Colourful Chess Pieces](https://opengameart.org/content/colorful-chess-pieces)
--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,68 @@
-#include <iostream>
-#include <string>
-#include <functional>
+#include <SFML\Graphics.hpp>
+#include <vector>
 #include <SFML\Graphics.hpp>
 
-#define DEFAULT_WINDOW_WIDTH 800
-#define DEFAULT_WINDOW_HEIGHT 600
-class Piece {
-    unsigned int position_x;
-    unsigned int position_y;
+class Entity {
+    
 };
 
-class Castle: public Piece {
+#define HORIZONTAL_CELL_AMOUNT 8
+#define VERTICAL_CELL_AMOUNT 8
 
-};
+class BoardCell {
+private:
+    sf::RenderWindow * m_window;
+    sf::Vector2f m_size;
+    sf::Color m_color;
+    sf::Vector2f m_position;
+public:
+    BoardCell(sf::RenderWindow * window, sf::Vector2f position, sf::Vector2f size, sf::Color color)
+        : m_window(window), m_position(position), m_size(size), m_color(color) {}
 
-class Teams {
-
+    void draw() {
+        sf::RectangleShape cell(m_size);
+        cell.setPosition(sf::Vector2f(m_position.x * m_size.x, m_position.y * m_size.y));
+        cell.setFillColor(m_color);
+        m_window->draw(cell);
+    }
 };
 
 class Board {
-    
-};
-
-class Text {
 private:
     sf::RenderWindow * m_window;
-    std::string m_text;
-    sf::Vector2f m_coordinates;
-    sf::FloatRect textRect;
-    sf::Text text;
-
-    bool isMouseOver() {
-        sf::Vector2f mousePosition = m_window->mapPixelToCoords(sf::Mouse::getPosition(*m_window));
-        return mousePosition.x > (m_coordinates.x - (textRect.width / 2.0f)) 
-            && mousePosition.x < (m_coordinates.x + (textRect.width / 2.0f))
-            && mousePosition.y > (m_coordinates.y - (textRect.height / 2.0f)) 
-            && mousePosition.y < (m_coordinates.y + (textRect.height / 2.0f));
-    }
-
-    bool isLeftMouseClick() {
-        return this->isMouseOver() && sf::Mouse::isButtonPressed(sf::Mouse::Left);
-    }
+    std::vector<BoardCell> m_cells;
     
+    void drawBoard() {
+        sf::Vector2u currentWindowSize = m_window->getSize();
+        sf::Vector2u cellSize(currentWindowSize.x / HORIZONTAL_CELL_AMOUNT, currentWindowSize.y / VERTICAL_CELL_AMOUNT); // Divides the window into cells
+
+        for (int horizontal_counter = 0; horizontal_counter < HORIZONTAL_CELL_AMOUNT; horizontal_counter++) {
+            for (int vertical_counter = 0; vertical_counter < VERTICAL_CELL_AMOUNT; vertical_counter++) {    
+                BoardCell cell(
+                    m_window,
+                    sf::Vector2f(horizontal_counter, vertical_counter),
+                    sf::Vector2f(cellSize.x, cellSize.y),
+                    ((horizontal_counter + vertical_counter) % 2) ? sf::Color {211, 211, 211} : sf::Color::White
+                );
+                cell.draw();
+                m_cells.push_back(cell);
+            }
+        }
+    }
+
+    void initializePieces() {
+
+    }
 public:
-    Text(sf::RenderWindow * window, std::string text, sf::Vector2f coordinates) : m_window(window), m_text(text), m_coordinates(coordinates) {}
-
-    void onHover(int action = 0) {
-        if (!this->isMouseOver()) {
-            return;
-        }
-
-        switch(action) {
-            case 1:
-                this->text.setFillColor(sf::Color::Red);
-                break;
-            default:
-                break;  
-        }
-    }
-
-    void onClick(int action = 0) {
-        if (!this->isLeftMouseClick()) {
-            return;
-        }
-
-        switch(action) {
-            case 1:
-                m_window->close();
-                break;
-            default:
-                break;
-        }
-    }
+    Board(sf::RenderWindow * window) : m_window(window) {}
 
     void draw() {
-        sf::Font font;
-        if (!font.loadFromFile("assets/fonts/Roboto/Roboto-Regular.ttf")) {
-            throw; // TODO: Return an error stating no fonts found here.
-        }
-
-        this->text.setFont(font);
-        this->text.setString(m_text);
-
-        // Centering the origin
-        textRect = this->text.getLocalBounds();
-        this->text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-        this->text.setPosition(m_coordinates.x, m_coordinates.y);
-        m_window->draw(this->text);
-    }
-};
-
-class Menu {
-private:
-    sf::RenderWindow * m_window;
-public:
-    Menu(sf::RenderWindow * window): m_window(window) {}
-
-    void draw() {
-        sf::Vector2u windowSize = m_window->getSize();
-        sf::Vector2u cellSize(windowSize.x / 8, windowSize.y / 20); // Divides the window into cells
-
-        Text title(m_window, "Moe Chess!", sf::Vector2f(cellSize.x * 4, cellSize.y * 5));
-        title.draw();
-        
-        Text start(m_window, "Start Game", sf::Vector2f(cellSize.x * 4, cellSize.y * 10));
-        start.draw();
-
-        Text quit(m_window, "Quit", sf::Vector2f(cellSize.x * 4, cellSize.y * 12));
-        quit.draw();
+        this->drawBoard();
     }
 };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT), "Moe Chess!", sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Moe Chess!", sf::Style::Close);
 
     while (window.isOpen()) {
 
@@ -122,10 +72,11 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        Menu menu(&window);
+
+        Board main_board(&window);
 
         window.clear(sf::Color::Black);
-        menu.draw();
+        main_board.draw();
         window.display();
     }
 
