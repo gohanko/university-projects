dfc50832742acdeb89f6be7536e66ed988d78863
gohanko <26451183+gohanko@users.noreply.github.com>
2021-11-27 15:55:48 +0800

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,56 @@
 #include <vector>
 #include <SFML\Graphics.hpp>
 
-class Entity {
-    
+class Piece {
+private:
+    sf::RenderWindow * m_window;
+    sf::Vector2i m_position;
+    bool is_dead = 0;
+
+    // Sprite size is 128x128
+    sf::IntRect spritePositionCalculator(int row, int column) {
+        const unsigned int sprite_size = 128;
+        sf::IntRect spriteRect(column * sprite_size, row * sprite_size, sprite_size, sprite_size);
+        return spriteRect;
+    };
+
+public:
+    Piece(sf::RenderWindow * window, sf::Vector2i position) : m_window(window), m_position(position) {}
+
+    void draw() {
+        // 128x128
+        sf::Texture texture;
+        if (!texture.loadFromFile("./assets/sprites/chess_pieces.png", this->spritePositionCalculator(1, 2))) {
+            throw;
+        }
+
+        sf::Sprite sprite;
+        sprite.setTexture(texture);
+        sprite.setPosition(m_position.x, m_position.y);
+        m_window->draw(sprite);
+    }
 };
 
-#define HORIZONTAL_CELL_AMOUNT 8
-#define VERTICAL_CELL_AMOUNT 8
+class Team {
+private:
+    sf::RenderWindow * m_window;
+    enum chess_piece_type { KING, QUEEN, ROOK, KNIGHTS, BISHOPS, PAWNS};
+public:
+    Team(sf::RenderWindow * window) : m_window(window) {}
+
+    void draw() {
+
+    }
+};
 
 class BoardCell {
 private:
     sf::RenderWindow * m_window;
     sf::Vector2f m_size;
     sf::Color m_color;
-    sf::Vector2f m_position;
+    sf::Vector2i m_position;
 public:
-    BoardCell(sf::RenderWindow * window, sf::Vector2f position, sf::Vector2f size, sf::Color color)
+    BoardCell(sf::RenderWindow * window, sf::Vector2i position, sf::Vector2f size, sf::Color color)
         : m_window(window), m_position(position), m_size(size), m_color(color) {}
 
     void draw() {
@@ -27,12 +62,15 @@ public:
     }
 };
 
+#define HORIZONTAL_CELL_AMOUNT 8
+#define VERTICAL_CELL_AMOUNT 8
+
 class Board {
 private:
     sf::RenderWindow * m_window;
     std::vector<BoardCell> m_cells;
-    
-    void drawBoard() {
+
+    void initialiseBoard() {
         sf::Vector2u currentWindowSize = m_window->getSize();
         sf::Vector2u cellSize(currentWindowSize.x / HORIZONTAL_CELL_AMOUNT, currentWindowSize.y / VERTICAL_CELL_AMOUNT); // Divides the window into cells
 
@@ -40,7 +78,7 @@ private:
             for (int vertical_counter = 0; vertical_counter < VERTICAL_CELL_AMOUNT; vertical_counter++) {    
                 BoardCell cell(
                     m_window,
-                    sf::Vector2f(horizontal_counter, vertical_counter),
+                    sf::Vector2i(horizontal_counter, vertical_counter),
                     sf::Vector2f(cellSize.x, cellSize.y),
                     ((horizontal_counter + vertical_counter) % 2) ? sf::Color {211, 211, 211} : sf::Color::White
                 );
@@ -50,14 +88,15 @@ private:
         }
     }
 
-    void initializePieces() {
+    void initialiseTeams() {
 
     }
 public:
     Board(sf::RenderWindow * window) : m_window(window) {}
 
     void draw() {
-        this->drawBoard();
+        this->initialiseBoard();
+        this->initialiseTeams();
     }
 };
 
