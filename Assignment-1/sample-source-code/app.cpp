67778c91bedd343c01d9f4122a6c72c60d01af7c
gohanko <26451183+gohanko@users.noreply.github.com>
2022-02-28 22:28:50 +0800

    Added student.txt file parser

--- Assignment-1/sample-source-code/app.cpp
+++ Assignment-1/sample-source-code/app.cpp
@@ -1,12 +1,14 @@
-#include	<iostream>
-#include	<cstdlib>
-#include	<cstdio>
-#include    <fstream>
-#include    <string>
-#include	"List.h"
-#include    "LibStudent.h"
-#include    "LibBook.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 
+#include "List.h"
+#include "LibStudent.h"
+#include "LibBook.h"
 
 using namespace std;
 
@@ -20,9 +22,56 @@ bool printStuWithSameBook(List *, char *);
 bool displayWarnedStudent(List *, List *, List *);
 int menu();
 
+List student_linked_list;
+
+vector<string> splitstring(string to_split, char seperator) {
+	stringstream streamData(to_split);
+	string value;
+	vector<string> outputArray;
+
+	while (getline(streamData, value, seperator)) {
+		outputArray.push_back(value);
+	}
+
+	return outputArray;
+}
+
+bool ReadFile(string filename) {
+	ifstream file;
+	file.open(filename, ios::in);
+	if (!file.is_open()) {
+		return false;
+	}
+
+	if (file.fail()) {
+		cout << "File not found!" << endl;
+		return false;
+	}
+
+	// student.txt parser
+	string line;
+	int current_line = 0;
+	while (file.good()) {
+		getline(file, line);
+		if (line.find_first_not_of(' ') == std::string::npos) { // Skips lines with only whitespace
+			continue;
+		}
+
+		current_line++;
+		vector<string> output = splitstring(line, '=');
+		cout << output[1] << endl;
+
+		if (current_line == 4) {
+			current_line = 0;
+		}
+	}
+
+	file.close();
+	return true;
+}
 
 int main() {
-	
+	ReadFile("../Assignment-1/sample-text-files/student.txt");
 
 	cout << "\n\n";
 	system("pause");
