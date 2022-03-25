5d1d564f2b18916add6b92232669497bf4f2d6ec
gohanko <26451183+gohanko@users.noreply.github.com>
2022-03-25 20:09:53 +0800

    Added polymorphism

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import LivingThing.LivingThingClass;
 import LivingThing.Human;
 
 public class App {
@@ -8,5 +9,16 @@ public class App {
         System.out.println("Is hungry: " + human.isHungry());
         System.out.println("Is sleeping: " + human.isSleeping());
         System.out.println("Is dead by age: " + human.isDeadByAge());
+
+        System.out.println("---------------------------------");
+
+        // Polymorphism: It uses the child's functions even though the 
+        // variable type is of the parent.
+        LivingThingClass human_2 = new Human("Yuki Mae", "1899-02-02");
+        System.out.println("Name: " + human_2.getName());
+        System.out.println("Age: " + human_2.calculateAge());
+        System.out.println("Is hungry: " + human_2.isHungry());
+        System.out.println("Is sleeping: " + human_2.isSleeping());
+        System.out.println("Is dead by age: " + human_2.isDeadByAge());
     }
 }
