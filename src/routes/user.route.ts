901f24abbc55fe3e43b03efea3e8fe99e8566d17
俞國忠 / Yii Kuo Chong <byu1628@proton.me>
2024-08-11 23:11:29 +0800

    a

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,13 +3,13 @@ import {
     register,
     login,
     logout,
-    resetPassword
+    changePassword
 } from '../controllers/user.controller'
 
 const userRouter = express.Router()
 userRouter.post('/user/register/', register)
 userRouter.post('/user/login/', login)
 userRouter.post('/user/logout/', logout)
-userRouter.post('/user/reset/', resetPassword)
+userRouter.post('/user/change_password/', changePassword)
 
 export default userRouter
