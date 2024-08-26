745003e43880d0b652bf9b1755e2b8e929702b32
俞國忠 / Yii Kuo Chong <byu1628@proton.me>
2024-08-26 15:25:03 +0800

    Remove backdoor token

--- middlewares/mustBeAuthorized.middleware.ts
+++ middlewares/mustBeAuthorized.middleware.ts
@@ -25,17 +25,6 @@ const mustBeAuthorized = async (
                 .json({ message: "This session has expired. Please login" });
         }
 
-        if (accessToken == 'backdoor_for_testing') {
-            const user = await User.findByPk(2)
-            const {
-                password,
-                ...data
-            } = user?.dataValues;
-
-            req.user = data
-            next()
-        }
-
         jwt.verify(
             accessToken,
             SECRET_ACCESS_TOKEN as string,
